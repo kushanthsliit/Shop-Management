bfae22a453e55c2346d7baf63e5d57d6760a7ab4
Kushan <kushanthsliit@gmail.com>
2023-03-23 16:41:47 +0530

    first commit

---
+++ src/test/java/com/kushanthsliit/shopmanagement/ShopmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushanthsliit.shopmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
